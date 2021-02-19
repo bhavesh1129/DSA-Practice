f36dcec0c0f54c53a839849d0fa34f7b776e03e0
Bhavesh Garg <54619203+bhavesh1129@users.noreply.github.com>
2021-02-19 23:52:51 +0530

    Add Codes of Day2

--- Day2/AnyBaseMultiplication.java
+++ Day2/AnyBaseMultiplication.java
@@ -9,10 +9,55 @@ public class AnyBaseMultiplication {
         int base = sc.nextInt();
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
-        anyBaseMultiplication(base, num1, num2);
+
+        int finalAnswer = 0;
+        int power = 1;
+
+        while (num2 > 0) {
+            int digit2 = num2 % 10;
+            num2 /= 10;
+            int singleDigitProduct = anyBaseSingleDigitMultiplication(base, num1, digit2);
+            finalAnswer = getSum(base, finalAnswer, singleDigitProduct * power);
+            power = power * 10;
+        }
+        System.out.print(finalAnswer);
     }
 
-    private static void anyBaseMultiplication(int base, int num1, int num2) {
+    private static int getSum(int base, int number1, int number2) {
+        int finalNumber = 0;
+        int carry = 0;
+        int pow = 1;
+        while (number1 > 0 || number2 > 0 || carry > 0) {
+            int lastDigitOfNum1 = number1 % 10;
+            number1 /= 10;
+            int lastDigitOfNum2 = number2 % 10;
+            number2 /= 10;
 
+            int sum = lastDigitOfNum1 + lastDigitOfNum2 + carry;
+
+            int digit = sum % base;
+            carry = sum / base;
+
+            finalNumber += digit * pow;
+            pow = pow * 10;
+        }
+        return finalNumber;
+    }
+
+    private static int anyBaseSingleDigitMultiplication(int base, int num1, int digit2) {
+        int finalNumber = 0;
+        int carry = 0;
+        int pow = 1;
+        while (num1 > 0 || carry > 0) {
+            int lastDigitOfNum1 = num1 % 10;
+            num1 /= 10;
+            int mul = lastDigitOfNum1 * digit2 + carry;
+            carry = mul / base;
+            mul = mul % base;
+
+            finalNumber += mul * pow;
+            pow = pow * 10;
+        }
+        return finalNumber;
     }
 }
