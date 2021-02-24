4e0b2d12d8f31660d55f3995391fe069d8132aaa
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-02-24 23:22:57 +0530

    Add Code of Day6

---
+++ Day6/WaveTraversal.java
@@ -0,0 +1,33 @@
+//Wave Transversal in An Array
+package Day6;
+
+import java.util.Scanner;
+
+public class WaveTraversal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rowOfArr = sc.nextInt();
+        int colOfArr = sc.nextInt();
+        int[][] arr = new int[rowOfArr][colOfArr];
+        for (int i = 0; i < rowOfArr; i++) {
+            for (int j = 0; j < colOfArr; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        waveTraversal(arr, rowOfArr, colOfArr);
+    }
+
+    private static void waveTraversal(int[][] arr, int rowOfArr, int colOfArr) {
+        for (int i = 0; i < colOfArr; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < arr.length; j++) {
+                    System.out.println(arr[j][i]);
+                }
+            } else {
+                for (int j = arr.length - 1; j >= 0; j--) {
+                    System.out.println(arr[j][i]);
+                }
+            }
+        }
+    }
+}
