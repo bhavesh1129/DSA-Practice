b226d3b4d806958e1ae26c6a620971bec2dcd791
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-03-02 23:06:30 +0530

    Update ToggleCase.java

--- Day11/ToggleCase.java
+++ Day11/ToggleCase.java
@@ -3,27 +3,21 @@ package Day11;
 
 import java.util.Scanner;
 
-public class ToggleCase {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        StringBuilder sb = new StringBuilder(str);
-        toggleCase(sb);
-    }
-
-    private static void toggleCase(StringBuilder sb) {
-        int i = 0;
-        while (i < sb.length()) {
-            char ch = sb.charAt(i);
-            if (ch >= 97 && ch <= 122) {
-                ch -= 32;
-                System.out.print(ch);
-            }
-            if (ch >= 65 && ch <= 90) {
-                ch += 32;
-                System.out.print(ch);
-            }
-            i++;
-        }
-    }
+	public static String toggleCase(String str){
+		StringBuilder ans = new StringBuilder();
+		for(int i = 0 ; i < str.length(); i++){
+			char ch = str.charAt(i);
+			if(ch >= 'a' && ch <= 'z'){
+				ans.append((char)(ch - 'a' + 'A') + "");
+			}else{
+				ans.append((char)(ch - 'A' +'a') + "");
+			}
+		}
+		return ans.toString();
+	}
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		System.out.println(toggleCase(str));
+	}
 }
