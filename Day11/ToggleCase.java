8e5bcb4c52e583b66618a40abab590c780cfb5a3
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-03-03 00:17:13 +0530

    Add Code of Day11

--- Day11/ToggleCase.java
+++ Day11/ToggleCase.java
@@ -3,21 +3,23 @@ package Day11;
 
 import java.util.Scanner;
 
-	public static String toggleCase(String str){
-		StringBuilder ans = new StringBuilder();
-		for(int i = 0 ; i < str.length(); i++){
-			char ch = str.charAt(i);
-			if(ch >= 'a' && ch <= 'z'){
-				ans.append((char)(ch - 'a' + 'A') + "");
-			}else{
-				ans.append((char)(ch - 'A' +'a') + "");
-			}
-		}
-		return ans.toString();
-	}
-	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
-		String str = scn.next();
-		System.out.println(toggleCase(str));
-	}
+public class ToggleCase {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        System.out.println(toggleCase(str));
+    }
+
+    private static String toggleCase(String str) {
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                ans.append((char) (ch - 'a' + 'A') + "");
+            } else {
+                ans.append((char) (ch - 'A' + 'a') + "");
+            }
+        }
+        return ans.toString();
+    }
 }
