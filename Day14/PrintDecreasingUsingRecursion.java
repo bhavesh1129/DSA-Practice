8f6271bf3ba927d4b5638db3b7ba641d9fb03acc
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-03-07 22:26:36 +0530

    Add Codes of Day14

---
+++ Day14/PrintDecreasingUsingRecursion.java
@@ -0,0 +1,19 @@
+// Print Decreasing Using Recursion
+package Day14;
+
+import java.util.Scanner;
+
+public class PrintDecreasingUsingRecursion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        printDecreasingUsingRecursion(num);
+    }
+
+    private static void printDecreasingUsingRecursion(int num) {
+        if (num > 0) {
+            System.out.println(num);
+            printDecreasingUsingRecursion(num - 1);
+        }
+    }
+}
