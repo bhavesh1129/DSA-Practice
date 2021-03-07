b360f03ac8734d3e0b4363dba3f64d4c3f265fe1
Bhavesh Garg <bhavesh0336.cse19@chitkara.edu.in>
2021-03-07 22:50:37 +0530

    Add Codes of Day17

---
+++ Day17/FactorialUsingRecursion.java
@@ -0,0 +1,21 @@
+//Factorial Using Recursion
+package Day17;
+
+import java.util.Scanner;
+
+public class FactorialUsingRecursion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(factorialUsingRecursion(num));
+    }
+
+    private static int factorialUsingRecursion(int num) {
+        if (num == 1) {
+            return 1;
+        }
+        int fact = factorialUsingRecursion(num - 1);
+        int factorial = num * fact;
+        return factorial;
+    }
+}
